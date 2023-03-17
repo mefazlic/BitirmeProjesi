61ac734074d3b2c6325abcbb19fd35fbef224a25
Fazliç <fazlic407@gmail.com>
2023-03-17 18:29:21 +0300

    roll and dash are ready (no animation)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,19 +5,62 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public float speed;
+    private enum State
+    {
+        Normal,
+        Rolling,
+    }
+
+    public float speed = 6f;
+    public float dashSize = 3f;
+    public LayerMask dashLayerMask;
     public Rigidbody2D rb;
-    private Vector2 moveDirection;
+    private Vector3 moveDirection;
+    private Vector3 rollDirection;
+    private float rollSpeed;
+    private bool isDashButtonDown;
+    private State state;
+
+    private void Awake()
+    {
+        state = State.Normal;
+    }
 
     private void Update()
     {
         // input processing
-        ProcessInputs();
+        switch (state)
+        {
+            case State.Normal:
+                ProcessInputs();
+                break;
+            case State.Rolling:
+                float rollSpeedDrop = 2f;
+                rollSpeed -= rollSpeed * rollSpeedDrop * Time.deltaTime;
+
+                float rollSpeedMin = 8f;
+                if (rollSpeed < rollSpeedMin)
+                {
+                    state = State.Normal;
+                }
+                break;
+        }
+        
     }
     private void FixedUpdate()
     {
         // physics
-        Move();
+        switch (state)
+        {
+            case State.Normal:
+                turnFace();
+                Move();
+                Dash();
+                break;
+            case State.Rolling:
+                rb.velocity = rollDirection * rollSpeed;
+                break;
+        }
     }
 
     void ProcessInputs()
@@ -25,12 +68,37 @@ public class Player : MonoBehaviour
         float Xaxis = Input.GetAxisRaw("Horizontal");
         float Yaxis = Input.GetAxisRaw("Vertical");
 
-        moveDirection = new Vector2(Xaxis, Yaxis).normalized;
+        moveDirection = new Vector3(Xaxis, Yaxis).normalized;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDashButtonDown = true;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            rollDirection = moveDirection;
+            rollSpeed = 20f;
+            state = State.Rolling;
+        }
     }
     void Move()
     {
-        turnFace();
-        rb.velocity = new Vector2(moveDirection.x * speed, moveDirection.y * speed);
+        rb.velocity = moveDirection* speed;
+    }
+
+    void Dash()
+    {
+        if (isDashButtonDown)
+        {
+            Vector3 dashPosition = transform.position + moveDirection * dashSize;
+            RaycastHit2D rch2d = Physics2D.Raycast(transform.position, moveDirection, dashSize, dashLayerMask);
+            if (rch2d.collider != null)
+            {
+                dashPosition = rch2d.point;
+            }
+            rb.MovePosition(dashPosition);
+            isDashButtonDown = false;
+        }
     }
 
     void turnFace()
