6a492cc4f28af015b30bbd27379323b723723fba
A.E.BASOL <ahmetefe.basol@ogr.iuc.edu.tr>
2023-03-19 03:01:20 +0300

    Room Creation and Optimized Dungeon Manager

--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using static System.Net.WebRequestMethods;
 
+public enum DungeonType { Caverns, Rooms }
+
 public class DungeonManager : MonoBehaviour
 {
     public GameObject[] randomItems; // Holds the items that spawn randomly
@@ -13,6 +15,8 @@ public class DungeonManager : MonoBehaviour
 
     public int totalFloorCount;
     [UnityEngine.Range(0, 100)] public int itemSpawnPercent;
+
+    public DungeonType dungeonType;
 
     [HideInInspector] public float minX, maxX, minY, maxY; // floors bounds (min x,y and max x,y)
 
@@ -22,8 +26,18 @@ public class DungeonManager : MonoBehaviour
     void Start()
     {
         floorMask = LayerMask.GetMask("Floor");
-        RandomWalker();
         wallMask = LayerMask.GetMask("Wall");
+
+
+        switch(dungeonType)
+        {
+            case DungeonType.Caverns: RandomWalker(); break;
+            case DungeonType.Rooms: RoomWalker(); break;
+        }
+
+
+
+
 
     }
 
@@ -44,31 +58,88 @@ public class DungeonManager : MonoBehaviour
 
         while (floorList.Count < totalFloorCount)
         {
-            switch (Random.Range(1, 5))
-            {
-                case 1: curPos += Vector3.up; break;
-                case 2: curPos += Vector3.right; break;
-                case 3: curPos += Vector3.down; break;
-                case 4: curPos += Vector3.left; break;
-            }
+            curPos += RandomDirection();
 
-            bool inFloorList = false;
-            for (int i = 0; i < floorList.Count; i++) // check if the current position is already in the list of floors
-            {
-                if (Vector3.Equals(curPos, floorList[i]))
-                {
-                    inFloorList = true;
-                    break;
-                }
-            }
-
-            if (!inFloorList)
+            if (!InFloorList(curPos))
             {
                 floorList.Add(curPos);
             }
 
         }
 
+        StartCoroutine(DelayProgress());
+
+    }
+
+    void RoomWalker()
+    {
+        Vector3 curPos = Vector3.zero;
+
+        floorList.Add(curPos); // add the current position to the list of floors
+
+        while (floorList.Count < totalFloorCount)
+        {
+            Vector3 walkDir = RandomDirection();
+            int walkLenght = Random.Range(9, 18);
+
+            for(int i = 0; i < walkLenght; i++)
+            {
+                if (!InFloorList(curPos + walkDir))
+                {
+                    floorList.Add(curPos + walkDir);
+                }
+
+                curPos += walkDir; // do this until you reach the walk lenght of room
+            }
+
+            int width = Random.Range(1, 5);
+            int height = Random.Range(1,5);
+
+            for(int w = -width; w <= width; w++) 
+            {
+                for (int h = -height; h <= height; h++)
+                {
+                    Vector3 offSet = new Vector3(w, h, 0);
+                    if (!InFloorList(curPos + offSet))
+                    {
+                        floorList.Add(curPos + offSet);
+                    }
+                }
+            }
+        }
+
+        StartCoroutine(DelayProgress());
+
+    }
+
+    Vector3 RandomDirection()
+    {
+        switch (Random.Range(1, 5))
+        {
+            case 1: return Vector3.up;
+            case 2: return Vector3.right;
+            case 3: return Vector3.down; 
+            case 4: return Vector3.left; 
+        }
+
+        return Vector3.zero; // Unity want to return something, even if this code doesn't mean anything it satisfies the return error
+    }
+
+    bool InFloorList(Vector3 myPos)
+    {
+        for (int i = 0; i < floorList.Count; i++) // check if the current position is already in the list of floors
+        {
+            if (Vector3.Equals(myPos, floorList[i]))
+            {
+                return true;
+            }
+        }
+
+        return false;  
+    }
+
+    IEnumerator DelayProgress() // spawn exit
+    {
         for (int i = 0; i < floorList.Count; i++)
         {
             GameObject goTile = Instantiate(tileSpawnerPrefab, floorList[i], Quaternion.identity) as GameObject;
@@ -76,13 +147,6 @@ public class DungeonManager : MonoBehaviour
             goTile.transform.SetParent(transform);
         }
 
-        StartCoroutine(DelayProgress());
-
-    }
-
-    // spawn exit
-    IEnumerator DelayProgress()
-    {
         while (FindObjectsOfType<TileSpawner>().Length > 0)
         {
             // TileSpawner unit is converted to Floor, so we waiting for complete
@@ -128,6 +192,8 @@ public class DungeonManager : MonoBehaviour
         goDoor.name = exitPrefab.name;
         goDoor.transform.SetParent(transform);
     }
+
+
     void RandomItems(Collider2D hitFloor, Collider2D hitTop, Collider2D hitRight, Collider2D hitBottom, Collider2D hitLeft)
     {
 
