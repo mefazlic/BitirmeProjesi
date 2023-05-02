dad11a027fa8409452e6b7fe60083d39f6909a0a
EFE-PC\ahmet <ahmetefe.basol@ogr.iuc.edu.tr>
2023-05-02 19:03:02 +0300

    walkin&dash fix

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -12,16 +11,25 @@ public class Player : MonoBehaviour
         Rolling,
     }
 
-    public float speed = 6f;
-    public float dashSize = 3f;
-    public LayerMask dashLayerMask;
-    public Rigidbody2D rb;
-    private Vector3 moveDirection;
-    private Vector3 rollDirection;
-    private float rollSpeed;
-    private bool isDashButtonDown;
     private State state;
     private WeaponParent weaponParent;
+
+    LayerMask obstacleMask;
+    Vector2 targetPos;
+    Transform GFX, weapon;
+    float flipGFX, flipWeapon;
+    bool isMoving;
+    public float speed;
+    public float dashDistance;
+
+    private void Start()
+    {
+        obstacleMask = LayerMask.GetMask("Wall", "Enemy");
+        GFX = GetComponentInChildren<SpriteRenderer>().transform;
+        weapon = GetComponentInChildren<WeaponParent>().transform;
+        flipGFX = GFX.localScale.x;
+        flipWeapon = weapon.localScale.x;
+    }
 
     private void Awake()
     {
@@ -29,66 +37,71 @@ public class Player : MonoBehaviour
         weaponParent = GetComponentInChildren<WeaponParent>();
     }
 
-    private void Update()
+    void Update()
     {
-        // input processing
-        switch (state)
+        ProcessInputs();
+        if (state == State.Normal)
         {
-            case State.Normal:
-                ProcessInputs();
-                break;
-            case State.Rolling:
-                float rollSpeedDrop = 2f;
-                rollSpeed -= rollSpeed * rollSpeedDrop * Time.deltaTime;
+            float horz = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
+            float vert = System.Math.Sign(Input.GetAxisRaw("Vertical"));
 
-                float rollSpeedMin = 8f;
-                if (rollSpeed < rollSpeedMin)
+            if (Mathf.Abs(horz) > 0 || Mathf.Abs(vert) > 0)
+            {
+                if (Mathf.Abs(horz) > 0)
                 {
-                    state = State.Normal;
+                    GFX.localScale = new Vector2(flipGFX * horz, GFX.localScale.y);
+                    weapon.localScale = new Vector2(flipWeapon * horz, weapon.localScale.y);
                 }
-                break;
+
+                if (!isMoving)
+                {
+                    if (Mathf.Abs(horz) > 0)
+                    {
+                        targetPos = new Vector2(transform.position.x + horz, transform.position.y);
+                    }
+                    else if (Mathf.Abs(vert) > 0)
+                    {
+                        targetPos = new Vector2(transform.position.x, transform.position.y + vert);
+                    }
+
+                    Vector2 hitSize = Vector2.one * 0.8f;
+                    Collider2D hit = Physics2D.OverlapBox(targetPos, hitSize, 0, obstacleMask);
+
+                    if (!hit)
+                    {
+                        StartCoroutine(SmoothMove());
+                    }
+                }
+            }
         }
     }
+
+    IEnumerator SmoothMove()
+    {
+        isMoving = true;
+        while (Vector2.Distance(transform.position, targetPos) > 0.01f)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
     private void FixedUpdate()
     {
-        // physics
-        switch (state)
-        {
-            case State.Normal:
-                turnFace();
-                Move();
-                Dash();
-                break;
-            case State.Rolling:
-                rb.velocity = rollDirection * rollSpeed;
-                break;
-        }
     }
 
     void ProcessInputs()
     {
-        float Xaxis = Input.GetAxisRaw("Horizontal");
-        float Yaxis = Input.GetAxisRaw("Vertical");
-
-        moveDirection = new Vector3(Xaxis, Yaxis).normalized;
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            isDashButtonDown = true;
+            Attack();
         }
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
-            rollDirection = moveDirection;
-            rollSpeed = 20f;
-            state = State.Rolling;
+            Dash();
         }
-        if (Input.GetKeyDown(KeyCode.Mouse0)) {
-            Attack();
-        }
-    }
-    void Move()
-    {
-        rb.velocity = moveDirection * speed;
     }
 
     void Attack()
@@ -98,25 +111,35 @@ public class Player : MonoBehaviour
 
     void Dash()
     {
-        if (isDashButtonDown)
+        if (!isMoving && state == State.Normal)
         {
-            Vector3 dashPosition = transform.position + moveDirection * dashSize;
-            RaycastHit2D rch2d = Physics2D.Raycast(transform.position, moveDirection, dashSize, dashLayerMask);
-            if (rch2d.collider != null)
+            float horz = System.Math.Sign(Input.GetAxisRaw("Horizontal"));
+            float vert = System.Math.Sign(Input.GetAxisRaw("Vertical"));
+
+            if (Mathf.Abs(horz) > 0 || Mathf.Abs(vert) > 0)
             {
-                dashPosition = rch2d.point;
+                targetPos = new Vector2(
+                    transform.position.x + (Mathf.Abs(horz) > 0 ? horz * dashDistance : 0),
+                    transform.position.y + (Mathf.Abs(vert) > 0 ? vert * dashDistance : 0)
+                );
+
+                // Check for obstacles before teleporting
+                Vector2 hitSize = Vector2.one * 0.8f;
+                Collider2D hit = Physics2D.OverlapBox(targetPos, hitSize, 0, obstacleMask);
+
+                if (!hit)
+                {
+                    // Ensure there is enough distance from the enemy before teleporting
+                    Collider2D[] nearbyEnemies = Physics2D.OverlapCircleAll(targetPos, 0.5f, LayerMask.GetMask("Enemy"));
+
+                    if (nearbyEnemies.Length == 0)
+                    {
+                        transform.position = targetPos;
+                    }
+                }
             }
-            rb.MovePosition(dashPosition);
-            isDashButtonDown = false;
         }
     }
 
-    void turnFace()
-    {
-        if (moveDirection.x != 0)
-        {
-            transform.localScale = new Vector2(1 * moveDirection.x, transform.localScale.y);
-        }
-    }
 
 }
